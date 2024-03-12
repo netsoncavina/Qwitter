20831612629222d7c5804d8ab670fdde7b83fd0a
Netson Cavina <netsoncavina@gmail.com>
2024-03-12 17:05:41 -0300

    Desenvolvimento drawer esquerdo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,8 +8,32 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      :width="283"
+      bordered
+    >
+      <q-icon name="fas fa-dove q-pa-md" size="lg" color="primary" />
+      <q-list>
+        <q-item to="/" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+        </q-item>
+        <q-item to="/about" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold"
+            >About</q-item-section
+          >
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
